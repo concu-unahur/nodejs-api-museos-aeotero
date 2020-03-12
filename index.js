74f63cf0b12a1ce8e43faafc0478edf05ed3db3b
agustin <popal9193@gmail.com>
2020-03-12 18:57:31 -0300

    parte del martes hecha

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const superagent = require('superagent');
 
 function imprimirMuseos(error, respuesta) {
   if (error) {
-    throw new Error('algo se rompió', error);
+    throw new Error('no se pudo extraer informacion', error);
   }
   var dato = ("")
   const cantidad = respuesta.body.count;
@@ -12,15 +12,14 @@ function imprimirMuseos(error, respuesta) {
   // console.log(`Se encontraron ${cantidad} museos.`);
   // console.log(`El primer museo se llama ${museos[0].nombre}.`)
   
-  for (i in range (0, cantidad)) {
-      dato = dato + nombreDireccion(museos[i]) + "\n" // VER ACA EL TEMA DEL FOR LOOP Y YA ESTARIA
-  }
+  respuesta.body.results.forEach(
+    u => dato = dato + nombreDireccion(u) + "\n"
+  )  // VER ACA EL TEMA DEL FOR LOOP Y YA ESTARIA
 
-  fs.writeFile("museos.txt", dato , terminar(error));
+  fs.writeFile("museos.txt", dato , despuesDeEscribir(error));
 
   // const prueba = nombreDireccion(respuesta.body.results[0]);
   // console.log(prueba); // prueba de la linea de texto 
-
 
 }
 
@@ -31,7 +30,7 @@ superagent
   .end(imprimirMuseos)
 
 
-// EJEMPLO PARA ESCRIBIR
+// // EJEMPLO PARA ESCRIBIR
 const fs = require ('fs');
 
 function despuesDeEscribir(error) {
@@ -41,10 +40,10 @@ function despuesDeEscribir(error) {
   console.log("anda a leer tu archivo")
 }
 
-const cancion = `hola\nchau`
+// const cancion = `hola\nchau`
 
-fs.writeFile("saludos.txt", cancion , despuesDeEscribir);
-// EJEMPLO PARA ESCRIBIR 
+// fs.writeFile("saludos.txt", cancion , despuesDeEscribir);
+// // EJEMPLO PARA ESCRIBIR 
 
 function nombreDireccion(museo) { // museo = respuesta.body.results[i]
   const nombre = museo.nombre;
@@ -54,9 +53,9 @@ function nombreDireccion(museo) { // museo = respuesta.body.results[i]
   return `${nombre} (${direccion}). Por cualquier consulta comunicarse al ${telefono} .` ;
 }
 
-function terminar(error) {
-  if (error) {
-    throw new Error("no se pudo escribir", error) ;
-  }
-  console.log("ya se escribio el archivo")
-}
+// function terminar(error) {
+//   if (error) {
+//     throw new Error("no se pudo escribir", error) ;
+//   }
+//   console.log("ya se escribio el archivo")
+// }
