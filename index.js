c4704721534d931306240efd85ab10e70b68f45d
agustin <popal9193@gmail.com>
2020-03-12 19:22:10 -0300

    segunda parte empesada y hecho hasta simil museos

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
 const superagent = require('superagent');
-
-// NOMBRE (DIRECCION). Por cualquier consulta comunicarse al TELEFONO
-
-function imprimirMuseos(error, respuesta) {
-  if (error) {
-    throw new Error('no se pudo extraer informacion', error);
-  }
-  var dato = ("")
-  const cantidad = respuesta.body.count;
-  const museos = respuesta.body.results;
-  // console.log(`Se encontraron ${cantidad} museos.`);
-  // console.log(`El primer museo se llama ${museos[0].nombre}.`)
-  
-  respuesta.body.results.forEach(
-    u => dato = dato + nombreDireccion(u) + "\n"
-  )  // VER ACA EL TEMA DEL FOR LOOP Y YA ESTARIA
-
-  fs.writeFile("museos.txt", dato , despuesDeEscribir(error));
-
-  // const prueba = nombreDireccion(respuesta.body.results[0]);
-  // console.log(prueba); // prueba de la linea de texto 
-
-}
+const fs = require ('fs');
 
 
 superagent
@@ -29,33 +7,67 @@ superagent
   .query({ format: 'json' })
   .end(imprimirMuseos)
 
+function imprimirMuseos(error, respuesta) {
+  if (error) {
+    throw new Error('no se pudo extraer informacion de museos', error);
+  }
+  var dato = ("")
+  const museos = respuesta.body.results;
+  
+  museos.forEach(
+    u => dato = dato + nombreDireccion(u) + "\n"
+  ) 
 
-// // EJEMPLO PARA ESCRIBIR
-const fs = require ('fs');
+  fs.writeFile("museos.txt", dato , terminar(error, "museos.txt"));
 
-function despuesDeEscribir(error) {
+}
+
+function terminar(error, archivo) {
   if (error) {
     throw new Error("no se pudo escribir", error) ;
   }
-  console.log("anda a leer tu archivo")
+  console.log(`ya se escribio el archivo ${archivo}`)
 }
+
+function nombreDireccion(dato) { // museo = respuesta.body.results[i]
+  const nombre = dato.nombre;
+  const direccion = dato.direccion;
+  const telefono = dato.telefono;
+
+  return `${nombre} (${direccion}). Por cualquier consulta comunicarse al ${telefono} .` ;
+}
+
+// // EJEMPLO PARA ESCRIBIR
+
+// function despuesDeEscribir(error) {
+//   if (error) {
+//     throw new Error("no se pudo escribir", error) ;
+//   }
+//   console.log("anda a leer tu archivo")
+// }
 
 // const cancion = `hola\nchau`
 
 // fs.writeFile("saludos.txt", cancion , despuesDeEscribir);
 // // EJEMPLO PARA ESCRIBIR 
 
-function nombreDireccion(museo) { // museo = respuesta.body.results[i]
-  const nombre = museo.nombre;
-  const direccion = museo.direccion;
-  const telefono = museo.telefono;
 
-  return `${nombre} (${direccion}). Por cualquier consulta comunicarse al ${telefono} .` ;
+superagent
+  .get('https://www.cultura.gob.ar/api/v2.0/organismos')
+  .query({ format: 'json' })
+  .end(imprimirOrganismos)
+
+function imprimirOrganismos(error, respuesta) {
+  if (error) {
+    throw new Error('no se pudo extraer informacion de organismos', error);
+  }
+  var dato = ("")
+  const organismos = respuesta.body.results;
+  
+  organismos.forEach(
+    u => dato = dato +"organismo: " + nombreDireccion(u) + "\n"
+  ) 
+
+  fs.writeFile("organismos.txt", dato , terminar(error, "organismos.txt"));
+
 }
-
-// function terminar(error) {
-//   if (error) {
-//     throw new Error("no se pudo escribir", error) ;
-//   }
-//   console.log("ya se escribio el archivo")
-// }
